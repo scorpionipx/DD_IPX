926deb1c92a0bc9dc10221223cbc45c9f96db7ca
DESKTOP-645ISFL\ScorpionIPX <scorpionipx@gmail.com>
2018-03-20 21:58:20 +0200

    Created project for 32a version

---
+++ licenta/cod/SunTracking32A/SunTracking32A/SunTracking32A/main.c
@@ -0,0 +1,18 @@
+/*
+ * SunTracking32A.c
+ *
+ * Created: 20-Mar-18 21:57:11
+ * Author : ScorpionIPX
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
